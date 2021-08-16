24be4fe9f523a3cfdff7e21910df1058c31e680b
Andres Caycho <andres.cytz@gmail.com>
2021-08-16 13:53:46 -0500

    FEM GraphQL Client Side Workshop

--- client/src/client.js
+++ client/src/client.js
@@ -1,9 +1,92 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
+import { setContext } from 'apollo-link-context'
 import gql from 'graphql-tag'
 
+/**
+ * Create a new apollo client and export as default
+ */
 
-const client = new ApolloClient()
+// new LOCAL typeDef
+const typeDefs = gql`
+  extend type User {
+    age: Int
+  }
+  extend type Pet {
+    vaccinated: Boolean!
+  }
+`
+// resolver to set LOCAL definiton of local typeDef
+const resolvers = {
+  User: {
+    // age(user, args, context, info, other) {
+    age() {
+      return 35
+    }
+  },
+  Pet: {
+    vaccinated() {
+      return true
+    }
+  }
+}
+
+const http = new HttpLink({
+  uri: 'http://localhost:4000/'
+  // uri: 'https://rickandmortyapi.com/graphql'
+})
+const delay = setContext(
+  request =>
+    new Promise((success, fail) => {
+      setTimeout(() => {
+        success()
+      }, 1500);
+    })
+)
+
+const cache = new InMemoryCache()
+
+const link = ApolloLink.from([
+  delay,
+  http
+])
+
+const client = new ApolloClient({
+  link,
+  cache,
+  resolvers,
+  typeDefs
+})
+
+
+// const link = new HttpLink({
+//   uri: 'http://localhost:4000/'
+//   // uri: 'https://rickandmortyapi.com/graphql'
+// })
+// const cache = new InMemoryCache()
+// const client = new ApolloClient({
+//   link,
+//   cache
+// })
+
+// const query = gql`
+//   {
+//     characters {
+//       results {
+//         name
+//         id
+//         __typename
+//       }
+//     }
+//   }
+// `
+// try {
+//   client.query({query})
+//   .then(result => console.log(result))
+// } catch (error) {
+//   console.log('Error: ', error)
+// }
 
 export default client
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,33 +1,132 @@
 import React, {useState} from 'react'
 import gql from 'graphql-tag'
-import PetBox from '../components/PetBox'
-import NewPet from '../components/NewPet'
 import { useQuery, useMutation } from '@apollo/react-hooks'
+import PetsList from '../components/PetsList'
+import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
+
+// const GET_ALL_PETS = gql`
+//   query getAllPets {
+//     pets {
+//     # result: pets { # (1)
+//       id
+//       name
+//       type
+//       img
+//       owner {
+//         id
+//         age @client # getting value defined on client but not on server
+//       }
+//     }
+//   }
+// `
+// const NEW_PET = gql`
+//   mutation CreateAPet($newPet: NewPetInput!) {
+//     addPet(input: $newPet) {
+//       id
+//       name
+//       type
+//       img
+//       # we should keep same fields that we want to refresh on GET_ALL_PETS
+//       owner {
+//         id
+//         age @client # getting value defined on client but not on server
+//       }
+//     }
+//   }
+// `
+
+// using fragments
+const PETS_FIELDS = gql`
+  fragment PetsFields on Pet {
+    id
+    name
+    type
+    img
+    vaccinated @client
+    owner {
+      id
+      age @client
+    }
+  }
+`
+const GET_ALL_PETS = gql`
+  query getAllPets {
+    pets {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`
+const NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
-  
+  const { data, loading, error } = useQuery(GET_ALL_PETS) // react hooks: woll render the page again
+  const [newPet, newPetPayload] = useMutation(NEW_PET, {
+    update(cache, { data: { addPet } }) {
+      const { pets } = cache.readQuery({ query: GET_ALL_PETS })
+      // const { result } = cache.readQuery({ query: GET_ALL_PETS }) // (1)
+      cache.writeQuery({
+        query: GET_ALL_PETS,
+        data: { pets: [addPet, ...pets] }
+        // data: { result: [addPet, ...result] } // (1)
+      })
+    },
+    optimisticResponse: {
+      __typename: 'Mutation',
+      addPet: {
+        __typename: 'Pet',
+        id: Math.floor(Math.random() * 1000) + '',
+        name: '...',
+        type: '...',
+        img: 'https://via.placeholder.com/300',
+        owner: {
+          __typename: 'User',
+          id: Math.floor(Math.random() * 1000) + '',
+          age: 0
+        }
+      }
+    }
+  })
+
   const onSubmit = input => {
     setModal(false)
+    newPet({
+      variables: { newPet: input } //,
+      /* you can also call optimisticResponse here, it will have priority */
+      // optimisticResponse: {
+      //   __typename: 'Mutation',
+      //   addPet: {
+      //     __typename: 'Pet',
+      //     id: Math.floor(Math.random() * 1000) + '',
+      //     name: input.name,
+      //     type: input.type,
+      //     img: 'https://via.placeholder.com/300'
+      //   }
+      // }
+    })
+  }
+  
+  /* dont forget this, otherwise app will break. If loading true data is undefined */
+  // if (loading || newPetPayload.loading) {
+  if (loading) { /* for optimisticResponse remove mutation loading */
+    return <Loader/>
   }
 
-  const petsList = pets.data.pets.map(pet => (
-    <div className="col-xs-12 col-md-4 col" key={pet.id}>
-      <div className="box">
-        <PetBox pet={pet} />
-      </div>
-    </div>
-  ))
-  
+  if (error || newPetPayload.error) {
+    return <p>Error!</p>
+  }
+
   if (modal) {
-    return (
-      <div className="row center-xs">
-        <div className="col-xs-8">
-          <NewPet onSubmit={onSubmit} onCancel={() => setModal(false)}/>
-        </div>
-      </div>
-    )
+    return <NewPetModal onSubmit={onSubmit} onCancel={ () => setModal(false) } />
   }
 
   return (
@@ -39,14 +138,12 @@ export default function Pets () {
           </div>
 
           <div className="col-xs-2">
-            <button onClick={() => setModal(true)}>new pet</button>
+            <button onClick={ () => setModal(true) }>new pet</button>
           </div>
         </div>
       </section>
       <section>
-        <div className="row">
-          {petsList}
-        </div>
+        <PetsList pets={ data.pets }/>
       </section>
     </div>
   )
